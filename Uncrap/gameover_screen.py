94bb655b548ceaf5b91da1ea194e1e7952c973e9
Gabriela Giosa <gabichoichit@gmail.com>
2019-05-28 11:08:14 -0300

    Fiz a tela de game over

---
+++ Uncrap/gameover_screen.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 28 10:23:12 2019
+
+@author: gabig_000
+"""
+
+import pygame
+import random
+from os import path
+
+from config import img_dir, BLACK, FPS, INIT, QUIT, YELLOW, HEIGHT, WIDTH
+
+def help_screen(screen):
+    
+    score_font = pygame.font.Font(path.join(fnt_dir, "PressStart2P.ttf"), 28)
+    
+    # Variável para o ajuste de velocidade
+    clock = pygame.time.Clock()
+
+    # Carrega o fundo da tela de ajuda
+    background = pygame.image.load(path.join(img_dir, 'Congrats.png')).convert()
+    background_rect = background.get_rect()
+
+    running = True
+    while running:
+        
+        # Ajusta a velocidade do jogo.
+        clock.tick(FPS)
+        
+        # Processa os eventos (mouse, teclado, botão, etc).
+        for event in pygame.event.get():
+            # Verifica se foi fechado.
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_x:
+                    state = QUIT
+                    running = False
+            
+
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_p:
+                    state = INIT
+                    running = False
+            
+        # A cada loop, redesenha o fundo e os sprites
+        screen.fill(BLACK)
+        screen.blit(background, background_rect)
+
+        text_surface = score_font.render("Oi", True, YELLOW)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (WIDTH / 2,  HEIGHT / 2)
+        screen.blit(text_surface, text_rect)
+
+        # Depois de desenhar tudo, inverte o display.
+        pygame.display.flip()
+
+    return state
